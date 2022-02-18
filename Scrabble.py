abcbfb5953e841111f9f49ca611f7b1210dfc032
Shelley Wilson <85369484+shelleywilsonaz@users.noreply.github.com>
2022-02-18 09:54:26 -0700

    Add files via upload

---
+++ Scrabble.py
@@ -0,0 +1,45 @@
+"""
+Scrabble
+Shelley Wilson
+2-17-22
+
+Codecadmey Project: Create a Scrabble game that takes in letters and point
+values in a list and combine them into a dictionary to score words.
+Then, score a game with multiple players to see who is winning the game
+"""
+
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+letter_to_points = {
+  key:value 
+  for key, value 
+  in zip(letters, points)
+  }
+letter_to_points[" "] = 0
+def score_word(word):
+  point_total = 0
+  for letter in word:
+    point_total += letter_to_points.get(letter, 0)  
+  return point_total
+brownie_points = score_word("BROWNIE")
+print(brownie_points)
+#Score a game
+player_to_words = {
+  "player1":["BLUE", "TENNIS", "EXIT"], 
+  "wordNerd":["EARTH", "EYES", "MACHINE"], 
+  "Lexi Con":["ERASER", "BELLY", "HUSKY"], 
+  "Prof Reader": ["ZAP", "COMA", "PERIOD"]
+  }
+player_to_points = {}
+for player, words in player_to_words.items():
+  player_points = 0
+  for word in words:
+    player_points += score_word(word)
+  player_to_points[player] = player_points
+print(player_to_points)
+
+
+
+
+
+
